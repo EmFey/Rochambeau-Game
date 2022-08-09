74f67c52b50a21434f111c4bfb0d75ede4abe8e9
EmFey <manufety15@gmail.com>
2022-08-09 16:46:20 +0200

    Added a function that plays a round of a game

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 function getComputerChoice() {
-    let pcHand = ["Rock", "Paper", "Scissors"];
+    let pcHand = ["ROCK", "PAPER", "SCISSORS"];
     return console.log(pcHand[Math.floor(Math.random() * pcHand.length)]);
 }
 
-getComputerChoice();
+function gameRound(playerSelection, pcSelection) {
+    playerSelection = prompt("Type Rock, Paper or Scissors").toUpperCase();
+    pcSelection = getComputerChoice();
+    
+    if ((playerSelection == "ROCK" && pcSelection == "SCISSORS") || (playerSelection == "PAPER" && pcSelection == "ROCK") || (playerSelection == "SCISSORS" && pcSelection == "PAPER")) {
+        console.log("Congrats! You Win!");
+    } else if (playerSelection === pcSelection) {
+        console.log("It is a draw!");
+    }
+    else { console.log(`HAHA! Loser! ${pcSelection} beat ${playerSelection}`); }
+}
+
+gameRound();
