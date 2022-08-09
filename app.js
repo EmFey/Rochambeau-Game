f96a289598ce8bb5e05f2dabbebffe773c0f45dc
EmFey <manufety15@gmail.com>
2022-08-09 17:12:04 +0200

    Added a function that runs when invalid output is entered

--- app.js
+++ app.js
@@ -3,17 +3,28 @@ function getComputerChoice() {
     return pcHand[Math.floor(Math.random() * pcHand.length)];
 }
 
+function invalidInput() {
+    return console.log("You lose because you dont follow instructions");
+}
+
 function gameRound(playerSelection, pcSelection) {
-    
+
     if ((playerSelection == "ROCK" && pcSelection == "SCISSORS") || (playerSelection == "PAPER" && pcSelection == "ROCK") || (playerSelection == "SCISSORS" && pcSelection == "PAPER")) {
         console.log(`Congrats! You Win! ${playerSelection} beat ${pcSelection}`);
     } else if (playerSelection === pcSelection) {
         console.log("It is a draw!");
+    } else if (playerSelection != "ROCK" || "PAPER" || "SCISSORS") {
+        invalidInput();
     }
     else { console.log(`HAHA! Loser! ${pcSelection} beat ${playerSelection}`); }
 }
 
-const playerSelection = prompt("Type Rock, Paper or Scissors").toUpperCase();
-const pcSelection = getComputerChoice();
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Type Rock, Paper or Scissors").toUpperCase();
+        const pcSelection = getComputerChoice();
+        console.log(gameRound(playerSelection, pcSelection));
+    }
+}
 
-console.log(gameRound(playerSelection, pcSelection));
+game();
