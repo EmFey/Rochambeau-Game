f40c88e23cdf129825bd9bf4ae63ab6a59a491df
EmFey <manufety15@gmail.com>
2022-11-15 22:02:50 +0200

    Cleaned up the code but still have to fix PC always wins

--- app.js
+++ app.js
@@ -9,69 +9,61 @@ const gameResult = document.querySelector(".game-result");
 let playerScore = 0;
 let pcScore = 0;
 
-
-
 function getComputerChoice() {
-    let pcHand = ["ROCK", "PAPER", "SCISSORS"];
-    return pcHand[Math.floor(Math.random() * pcHand.length)];
+  let pcHand = ["ROCK", "PAPER", "SCISSORS"];
+  return pcHand[Math.floor(Math.random() * pcHand.length)];
 }
-
 
 function gameRound(playerSelection, pcSelection) {
   if (playerSelection === pcSelection) {
     Tie();
   }
 
-  else if ( (playerSelection === 'ROCK' && pcSelection === 'SCISSORS') || (playerSelection === 'PAPER' && pcSelection === 'ROCK') || (playerSelection === 'SCISSORS' && pcSelection === 'PAPER')) {
+  if ( (playerSelection === 'ROCK' && pcSelection === 'SCISSORS') || (playerSelection === 'PAPER' && pcSelection === 'ROCK') || (playerSelection === 'SCISSORS' && pcSelection === 'PAPER')) {
     Win();
-    if (playerScore === 5) {
-      gameResult.classList.add(".won");
-      gameResult.textContent = "You won the game";
-      disableButtons();
-    }
   }
 
-  else ( (pcSelection === 'ROCK' && playerSelection === 'SCISSORS') || (pcSelection === 'PAPER' && playerSelection === 'ROCK') || (pcSelection === 'SCISSORS' && playerSelection === 'PAPER')) ;{
+  if ( (pcSelection === 'ROCK' && playerSelection === 'SCISSORS') || (pcSelection === 'PAPER' && playerSelection === 'ROCK') || (pcSelection === 'SCISSORS' && playerSelection === 'PAPER')) ;{
     Lose();
-    if (pcScore === 5) {
-      gameResult.classList.add(".lose");
-      gameResult.textContent = "Computer won the game";
-      disableButtons();
-    }
+  }
+}
+
+function Tie() {
+  result.textContent = "It's a draw";
+}
+
+function Win() {
+  playerScore++;
+  result.textContent = "You won";
+  userPoint.textContent = playerScore;
+  if (playerScore === 5) {
+    gameResult.textContent = "You won the game";
+    gameResult.classList.add("won");
+    disableButtons();
+  }
+}
+
+function Lose() {
+  pcScore++;
+  result.textContent = "You lost";
+  pcPoint.textContent = pcScore;
+  if (pcScore === 5) {
+    gameResult.textContent = "Computer won the game";
+    gameResult.classList.add("lose");
+    disableButtons();
   }
 }
 
 function disableButtons() {
-      r.disabled = true;
-      p.disabled = true;
-      s.disabled = true;
+  r.disabled = true;
+  p.disabled = true;
+  s.disabled = true;
 }
-
-
-function Tie() {
-    result.textContent = "It's a draw";
-}
-
-function Win() {
-    result.textContent = "You won";
-    playerScore++;
-    userPoint.textContent = playerScore;
-    pcPoint.textContent = pcScore;
-}
-
-function Lose() {
-    result.textContent = "You lost";
-    pcScore++;
-    userPoint.textContent = playerScore;
-    pcPoint.textContent = pcScore;
-}
-
-
-
+  
 function main() {
-    r.addEventListener("click", () => gameRound("ROCK", getComputerChoice()));
-    p.addEventListener("click", () => gameRound("PAPER", getComputerChoice()));
-    s.addEventListener("click", () => gameRound("SCISSORS", getComputerChoice()));
+  r.addEventListener("click", () => gameRound("ROCK", getComputerChoice()));
+  p.addEventListener("click", () => gameRound("PAPER", getComputerChoice()));
+  s.addEventListener("click", () => gameRound("SCISSORS", getComputerChoice()));
 }
-
+  
 main();
